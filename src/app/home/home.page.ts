1d40214d96968f96683fbea124dea0573af86712
Ratthasak Barungrath <rathsakdibarungrath@gmail.com>
2024-08-01 12:32:40 +0700

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,19 @@
+
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonGrid, IonCol, IonRow, IonButton, IonIcon} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { trash } from 'ionicons/icons';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonGrid, IonCol, IonRow, IonButton, IonIcon],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({trash});
+  }
 }
